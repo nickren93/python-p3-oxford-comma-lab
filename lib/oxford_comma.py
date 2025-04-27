13267cc6d367c14a13f805bc254caefcf0f25ed6
Sitong <nickren93@gmail.com>
2025-04-27 16:07:46 -0400

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) < 2:
+        return ",".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[0: -1]) + f", and {items[-1]}"
